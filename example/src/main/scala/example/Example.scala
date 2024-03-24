81b24c30fe1be4375d3b92654cdc6756c394e972
Frank Wagner <agilecoderfrank@gmail.com>
2024-03-24 23:34:10 +0100

    scala2 macro implementations for remaining macros

--- example/src/main/scala/example/Example.scala
+++ example/src/main/scala/example/Example.scala
@@ -1,7 +1,9 @@
 package example
 
-import frawa.inlinefiles.InlineFiles.inlineTextFile
+import frawa.inlinefiles.InlineFiles._
 
 object Example {
-  val inlinedText = inlineTextFile("./test-files/inlined.txt")
+  val inlinedText          = inlineTextFile("./test-files/inlined.txt")
+  val inlinedTextFiles     = inlineTextFiles("./test-files/folder", ".txt")
+  val inlinedDeepTextFiles = inlineDeepTextFiles("./test-files/folder", ".txt")
 }
--- example/src/test/scala/example/ExampleTest.scala
+++ example/src/test/scala/example/ExampleTest.scala
@@ -5,6 +5,28 @@ import munit.FunSuite
 class ExampleTest extends FunSuite {
 
   test("inlined text") {
-    assertEquals("This file content will be inlined.", Example.inlinedText)
+    assertEquals(Example.inlinedText, "This file content will be inlined.")
   }
+
+  test("inline files in a folder") {
+    assertEquals(
+      Example.inlinedTextFiles,
+      Map(
+        "inlined1.txt" -> "First",
+        "inlined2.txt" -> "Second"
+      )
+    )
+  }
+
+  test("inline files in nested folders") {
+    assertEquals(
+      Example.inlinedDeepTextFiles,
+      Map(
+        "inlined1.txt"      -> "First",
+        "inlined2.txt"      -> "Second",
+        "deep/inlined3.txt" -> "Third\nand more"
+      )
+    )
+  }
+
 }
